b0cb6729c2824501d3bcceee7ce294f327f352e6
yajatkaul <mc.crafter1973@gmail.com>
2026-02-25 23:29:14 +0530

    Fixed add to party crash

--- common/src/main/java/com/github/pokeclash/cobblewand/network/server/handler/PokemonAddHandler.java
+++ common/src/main/java/com/github/pokeclash/cobblewand/network/server/handler/PokemonAddHandler.java
@@ -1,6 +1,9 @@
 package com.github.pokeclash.cobblewand.network.server.handler;
 
 import com.cobblemon.mod.common.Cobblemon;
+import com.cobblemon.mod.common.api.pokemon.PokemonProperties;
+import com.cobblemon.mod.common.api.pokemon.PokemonPropertyExtractor;
+import com.cobblemon.mod.common.pokemon.Pokemon;
 import com.github.pokeclash.cobblewand.network.server.packet.PokemonAddPacket;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.server.level.ServerPlayer;
@@ -8,7 +11,9 @@ import net.minecraft.server.level.ServerPlayer;
 public class PokemonAddHandler {
     public static void handle(PokemonAddPacket pokemonAddPacket, NetworkManager.PacketContext packetContext) {
         if (packetContext.getPlayer() instanceof ServerPlayer serverPlayer) {
-            Cobblemon.INSTANCE.getStorage().getParty(serverPlayer).add(pokemonAddPacket.pokemon());
+            PokemonProperties properties = pokemonAddPacket.pokemon().createPokemonProperties(PokemonPropertyExtractor.ALL);
+            Pokemon pokemon = properties.create();
+            Cobblemon.INSTANCE.getStorage().getParty(serverPlayer).add(pokemon);
         }
     }
 }
